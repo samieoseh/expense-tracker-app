a52e3767b7bdb6f8601081a00ee53bbd7cd3688a
Samuel Oseh <samieoseh@gmail.com>
2023-12-25 19:20:42 +0100

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
