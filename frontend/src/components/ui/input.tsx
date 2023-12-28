84611626399d3d9670e227bdab1dfb286bc78c93
Samuel Oseh <samieoseh@gmail.com>
2023-12-28 18:45:15 +0100

    finished auth mobile screen

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 import { cn } from "../../lib/utils";
-
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
@@ -11,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#7f7e7e] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground  disabled:cursor-not-allowed disabled:opacity-50 focus:ring-2 focus:ring-secondary focus:outline-none",
           className
         )}
         ref={ref}
